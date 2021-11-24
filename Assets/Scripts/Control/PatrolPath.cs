921f4db6fd922f92c8ed54366b11ea2bacdc131f
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2021-11-24 17:36:17 +0300

    53.waypoint loops

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -6,9 +6,15 @@ namespace RPG.Control {
         [SerializeField] private float radius = .3f;
 
         private void OnDrawGizmos() {
-            foreach (Transform t in transform) {
-                Gizmos.DrawSphere(t.position, radius);
+            for (int i = 0; i < transform.childCount; i++) {
+                Gizmos.DrawSphere(GetWaypoint(i), radius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(i + 1));
             }
+        }
+
+        private Vector3 GetWaypoint(int i) {
+            i = i == transform.childCount ? 0 : i;
+            return transform.GetChild(i).position;
         }
     }
 }
