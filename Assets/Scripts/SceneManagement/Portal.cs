786ebbd9df231f76d426af77f7fb142be2f88d83
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2021-11-30 11:39:59 +0300

    74. cross scene reference

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -7,7 +7,12 @@ using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
+        public enum DestinationIdentifier {
+            A,B,C,D,E
+        }
+        
         [SerializeField] private int sceneInd = -1;
+        [SerializeField] private DestinationIdentifier destination;
         public Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other) {
@@ -17,6 +22,11 @@ namespace RPG.SceneManagement {
         }
 
         private IEnumerator Transition() {
+            if (sceneInd < 0) {
+                Debug.LogError("Scene to load is not set.");
+                yield break;
+            }
+            
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneInd);
             
@@ -26,7 +36,7 @@ namespace RPG.SceneManagement {
         }
 
         private void SetPlayerPosition() {
-            Transform spawnPoint = FindObjectsOfType<Portal>().First(p => p != this).spawnPoint.transform;
+            Transform spawnPoint = FindObjectsOfType<Portal>().First(p => p != this && p.destination == destination).spawnPoint.transform;
             Transform player = GameObject.FindWithTag("Player").transform;
             player.GetComponent<NavMeshAgent>().Warp(spawnPoint.position);
             player.rotation =spawnPoint.rotation;
