8e1f353e11e7659e998d6c19b0bb4f9691eb2b94
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2021-12-22 14:58:44 +0300

    128. pickup respawning

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,14 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 namespace RPG.Combat {
     public class WeaponPickup : MonoBehaviour {
+        [SerializeField] private float hideTime = 5f;
         [SerializeField] private WeaponSO weaponSo;
-    
+        
+        private Collider collider;
+
+        private void Awake() {
+            collider = GetComponent<Collider>();
+        }
+
         private void OnTriggerEnter(Collider other) {
             if (other.CompareTag("Player")) {
                 other.GetComponent<Fighter>().EquipWeapon(weaponSo);
-                Destroy(gameObject);
+                StartCoroutine(HideRoutine());
             }
         }
+
+        private IEnumerator HideRoutine() {
+            TogglePickup(false);
+            yield return new WaitForSeconds(hideTime);
+            TogglePickup(true);
+        }
+
+        private void TogglePickup(bool isEnable) {
+            collider.enabled = isEnable;
+
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(isEnable);
+            }
+        }
+        
     }
 }
