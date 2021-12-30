dd1a6c4d0705d75f8f1e7da53797b70c91eb57b3
Rakhman Gaibatov <gaibatov.ra@yandex.ru>
2021-12-30 17:05:11 +0300

    161. calculating a navmesh path

--- Assets/Scripts/RPG/Control/PlayerController.cs
+++ Assets/Scripts/RPG/Control/PlayerController.cs
@@ -9,7 +9,6 @@ using UnityEngine.EventSystems;
 
 namespace RPG.Control {
     public class PlayerController : MonoBehaviour {
-
         [Serializable]
         struct CursorMapping {
             public CursorType type;
@@ -17,6 +16,7 @@ namespace RPG.Control {
             public Vector2 hotspot;
         }
 
+        [SerializeField] private float maxNavmeshPathLength = 15f;
         [SerializeField] private CursorMapping[] cursorMappings;
 
         private Mover mover;
@@ -61,32 +61,45 @@ namespace RPG.Control {
 
             return false;
         }
-        
+
         private bool InteractWithMovement() {
             if (RaycastNavMesh(out Vector3 target)) {
                 if (Input.GetMouseButton(0)) {
                     mover.StartMoveAction(target);
                 }
+
                 SetCursor(CursorType.Movement);
                 return true;
             }
+
             return false;
         }
 
         private bool RaycastNavMesh(out Vector3 target) {
             Ray ray = GetMouseRay();
-            if (!Physics.Raycast(ray, out RaycastHit hit)) {
-                target = Vector3.zero;
-                return false;
-            }
-
-            if (NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, .5f, NavMesh.AllAreas)) {
-                target = navMeshHit.position;
-                return true;
-            }
-
             target = Vector3.zero;
-            return false;
+            if (!Physics.Raycast(ray, out RaycastHit hit) ||
+                !NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, .5f, NavMesh.AllAreas)) return false;
+
+            target = navMeshHit.position;
+
+            NavMeshPath navMeshPath = new NavMeshPath();
+            if (!NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, navMeshPath) ||
+                navMeshPath.status != NavMeshPathStatus.PathComplete) return false;
+            
+            if (CalculatePathLength(navMeshPath) > maxNavmeshPathLength) return false;
+            
+            return true;
+        }
+
+        private float CalculatePathLength(NavMeshPath navMeshPath) {
+            Vector3 accumulator = Vector3.zero;
+            Vector3[] corners = navMeshPath.corners;
+            for (int i = 1; i < corners.Length; i++) {
+                accumulator += corners[i] - corners[i - 1];
+            }
+
+            return accumulator.magnitude;
         }
 
         private bool InteractWithUI() {
